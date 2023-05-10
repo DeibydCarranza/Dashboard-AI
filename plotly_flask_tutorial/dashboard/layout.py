4eb10455e81050cc2044ada6ff26bca82e06283e
DeibydCarranza <deibydcarranza@outlook.com>
2023-05-09 19:04:40 -0600

    slidebar con efecto

--- plotly_flask_tutorial/dashboard/layout.py
+++ plotly_flask_tutorial/dashboard/layout.py
@@ -1,29 +1,34 @@
-"""Plotly Dash HTML layout override."""
+import os
+
+with open(os.path.join(os.path.dirname(__file__), '..', 'templates', 'navbar.jinja2'), 'r') as file:
+    navbar = file.read()
 
 html_layout = """
 <!DOCTYPE html>
     <html>
         <head>
-            {%metas%}
+            {%metas%}            
             <title>{%title%}</title>
             {%favicon%}
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
             {%css%}
-        </head>
-        <body class="dash-template">
+            </head>
+            <body class="dash-template">
             <header>
-              <div class="nav-wrapper">
+                <div class="nav-wrapper">
                 <a href="/">
                     <img src="/static/img/logo.png" class="logo" />
-                    <h1>Plotly Dash Flask Tutorial</h1>
-                  </a>
-                <nav>
-                </nav>
-            </div>
+                    <h1>Asociación</h1>
+                </a>
+                """ + navbar + """
+                </div>
             </header>
             {%app_entry%}
             <footer>
                 {%config%}
                 {%scripts%}
+                <script type="text/javascript" src="../../static/js/barra.js"></script>
+
                 {%renderer%}
             </footer>
         </body>
