82ccc96c810d630be9e8a748f55181bf80a44a21
David Carranza <deibydcarranza@outllok.com>
2023-05-12 09:18:40 -0600

    Body dark

--- dashboard/static/js/barra.js
+++ dashboard/static/js/barra.js
@@ -1,52 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const body = document.querySelector('body'),
-          sidebar = body.querySelector('nav'),
-          toggle = body.querySelector(".toggle"),
-          searchBtn = body.querySelector(".search-box"),
-          modeSwitch = body.querySelector(".toggle-switch"),
-          modeText = body.querySelector(".mode-text"),
-          header = body.querySelector("header");
-  
-    // Al cargar la página, comprobar si el valor del darkmode ya está almacenado y aplicarlo si es necesario
-    if (localStorage.getItem("darkmode") === "true") {
-      body.classList.add("dark");
+  const body = document.querySelector('body'),
+        sidebar = body.querySelector('nav'),
+        toggle = body.querySelector(".toggle"),
+        searchBtn = body.querySelector(".search-box"),
+        modeSwitch = body.querySelector(".toggle-switch"),
+        modeText = document.querySelector(".mode-text"),
+        header = body.querySelector("header");
+
+  toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+  });
+
+  searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+  });
+
+  modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
       modeText.innerText = "Light mode";
-      if (header) { // Si existe el header, agregar la clase "dark"
+      if (header) { 
         header.classList.add("dark");
       }
     } else {
-      body.classList.remove("dark");
       modeText.innerText = "Dark mode";
-      if (header) { // Si existe el header, remover la clase "dark"
+      if (header) {
         header.classList.remove("dark");
       }
     }
-  
-    toggle.addEventListener("click", () => {
-      sidebar.classList.toggle("close");
-    })
-  
-    searchBtn.addEventListener("click", () => {
-      sidebar.classList.remove("close");
-    })
-  
-    modeSwitch.addEventListener("click", () => {
-      body.classList.toggle("dark");
-  
-      // Al hacer clic en el botón del modo oscuro, almacenar el valor actual en el navegador
-      if (body.classList.contains("dark")) {
-        localStorage.setItem("darkmode", "true");
-        modeText.innerText = "Light mode";
-        if (header) { // Si existe el header, agregar la clase "dark"
-          header.classList.add("dark");
-        }
-      } else {
-        localStorage.setItem("darkmode", "false");
-        modeText.innerText = "Dark mode";
-        if (header) { // Si existe el header, remover la clase "dark"
-          header.classList.remove("dark");
-        }
-      }
-    });
+    const paragraphs = document.querySelectorAll('p');
+    const h1s = document.querySelectorAll('h1');
+    const h2s = document.querySelectorAll('h2');
+    const h3s = document.querySelectorAll('h3');
+    paragraphs.forEach(p => p.classList.toggle('dark'));
+    h1s.forEach(h1 => h1.classList.toggle('dark'));
+    h2s.forEach(h2 => h2.classList.toggle('dark'));
+    h3s.forEach(h3 => h3.classList.toggle('dark'));
+    const contentContainer = document.querySelector('.content-container');
+    contentContainer.classList.toggle('dark');
   });
-  
+});
