006d0ce15bef1f667b99d6a183b960f0e5ac58e2
Diego Andres Ramirez Garcia <diego.unam.15@gmail.com>
2023-05-24 01:36:48 -0600

    normalizacion&escalamiento

--- application/routes.py
+++ application/routes.py
@@ -6,6 +6,7 @@ import json
 import plotly
 import plotly.express as px
 import os
+from sklearn.preprocessing import StandardScaler, MinMaxScaler 
 
 global path_file,path_file_json
 path_file = os.path.join(os.path.dirname(__file__), './data/', 'file.csv')
@@ -15,7 +16,7 @@ path_file_json = os.path.join(os.path.dirname(__file__), '../', 'data.json')
 def index():    
     return render_template('index.jinja2')
 
-
+""" -------------- Algoritms ----------------"""
 
 @app.route('/apriori/')
 def apriori():
@@ -36,9 +37,12 @@ def metricas():
     return render_template('layout_Metricas.jinja2',
             title="Metricas")
 
+
+""" -------------- Methods ----------------"""
 # Decorador de carga para la carga y sobreescritura de los archivos
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
+    print("try...")
     if 'csv_file' not in request.files:
         return "No file uploaded"
     
@@ -78,3 +82,20 @@ def chart1():
 
     return render_template('index.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON)
 
+@app.route('/standarizar')
+def standarizar():
+    print('Estandarizando')
+    file = MinMaxScaler()
+    MEstandarizada = file.fit_transform(pd.read_csv(path_file)) 
+    DFrame_standar = pd.DataFrame(MEstandarizada) 
+    DFrame_standar.to_csv(path_file, index=False)
+    return "Estandarizados"
+
+@app.route('/normalizar')
+def normalizar():
+    print('Normalizando')
+    file = StandardScaler()
+    MEnormalizada = file.fit_transform(pd.read_csv(path_file)) 
+    DFrame_standar = pd.DataFrame(MEnormalizada) 
+    DFrame_standar.to_csv(path_file, index=False)
+    return "Normalizados"
