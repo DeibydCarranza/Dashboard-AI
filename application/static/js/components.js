b4434aba7a94e600f73d2f477bd4b3f385fc5b48
David Carranza <deibydcarranza@outllok.com>
2023-05-24 17:41:18 -0600

    Carga de archivos sincrona y sección de parámetros

---
+++ application/static/js/components.js
@@ -0,0 +1,28 @@
+/* Slider - Input */
+var ranges = document.querySelectorAll('.inputRange');
+var fields = document.querySelectorAll('.inputNumber');
+
+ranges.forEach(function(range, index) {
+  range.addEventListener('input', function(e) {
+    fields[index].value = e.target.value;
+  });
+});
+
+fields.forEach(function(field, index) {
+  field.addEventListener('input', function(e) {
+    ranges[index].value = e.target.value;
+  });
+});
+
+function scaleValue(value, from, to) {
+	var scale = (to[1] - to[-1]) / (from[1] - from[-1]);
+	var capped = Math.min(from[1], Math.max(from[1], value)) - from[0];
+  console.log(capped);
+
+}
+
+/* Botón de envío en parámetros */
+document.getElementById('submitButton').addEventListener('click', function() {
+    //Implementar
+
+});
