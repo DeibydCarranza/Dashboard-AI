bbe1f659ccf17c65abc3d34af7034b27a4fb119d
David Carranza <deibydcarranza@outllok.com>
2023-05-24 15:31:34 -0600

    Mezclando ramas 'Button' y 'Apriori'

--- application/Algorithms/Apriori/method.py
+++ application/Algorithms/Apriori/method.py
@@ -1,27 +1,24 @@
 import pandas as pd
 import plotly.graph_objects as go
 from apyori import apriori
-
+import json
+import plotly
 
 def process_dataGraph(transac_data):
-    #Se incluyen todas las transacciones en una sola lista
-    Transacciones = transac_data.values.reshape(-1).tolist() 
+    # Se incluyen todas las transacciones en una sola lista
+    Transacciones = transac_data.values.reshape(-1).tolist()
 
-    #Se crea una matriz (dataframe) usando la lista y se incluye una columna 'Frecuencia'
+    # Se crea una matriz (dataframe) usando la lista y se incluye una columna 'Frecuencia'
     Lista = pd.DataFrame(Transacciones)
     Lista['Frecuencia'] = 1
 
-    #Se agrupa los elementos
+    # Se agrupa los elementos
     Lista = Lista.groupby(by=[0], as_index=False).count().sort_values(by=['Frecuencia'], ascending=True)
     Lista['Porcentaje'] = (Lista['Frecuencia'] / Lista['Frecuencia'].sum())
-    Lista = Lista.rename(columns={0 : 'Item'})
+    Lista = Lista.rename(columns={0: 'Item'})
 
-    return img_gen(Lista)
-
-
-def img_gen(Lista):
-    fig = go.Figure([go.Bar(x=Lista['Frecuencia'], y=Lista['Item'], 
-                    orientation='h',marker=dict(color='green'))])
+    # Genera la gráfica utilizando Plotly
+    fig = go.Figure([go.Bar(x=Lista['Frecuencia'], y=Lista['Item'], orientation='h', marker=dict(color='green'))])
     fig.update_layout(
         title='Distribución de los datos',
         xaxis_title='Frecuencia',
@@ -32,7 +29,10 @@ def img_gen(Lista):
             size=12
         )
     )
-    return fig
+    #Convirtiendo la imagen a JSON
+    graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graph_json
 
 def application(dataSet, support, confidence, lift):
     dataSet = dataSet.stack().groupby(level=0).apply(list).tolist() 
@@ -59,5 +59,4 @@ def application(dataSet, support, confidence, lift):
     
     df_resultados = pd.DataFrame(resultados)
     return df_resultados
-
 
